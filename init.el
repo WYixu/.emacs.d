eee4fb6bfe920063b27d3e5663ce37abc4dfea72
Yixu Wang <yxwang@localhost.localdomain>
2024-09-24 23:31:29 +0800

    Update theme and visual

--- init.el
+++ init.el
@@ -1,3 +1,7 @@
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(when (file-exists-p custom-file)
+  (load custom-file))
+
 ;; Initialize package sources
 (require 'package)
 
@@ -23,18 +27,37 @@
 (tooltip-mode -1)
 (set-fringe-mode 10)
 
-(setq visual-bell t)
+(column-number-mode)
+(global-display-line-numbers-mode t)
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
-
-(set-face-attribute 'default nil :font "JetBrainsMonoNL Nerd Font")
-
-(use-package catppuccin-theme
+;; (use-package catppuccin-theme
+;;  :ensure t
+;;  :config
+;;  (load-theme 'catppuccin :no-confirm)
+;;  (setq catppuccin-flavor 'latte)
+;;  (catppuccin-reload))
+(use-package doom-themes
   :ensure t
   :config
-  (load-theme 'catppuccin :no-confirm)
-  (setq catppuccin-flavor 'latte)
-  (catppuccin-reload))
+  ;; Global settings (defaults)
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
+  (load-theme 'doom-one-light t)
+
+  ;; Enable flashing mode-line on errors
+  (doom-themes-visual-bell-config)
+  ;; Enable custom neotree theme (all-the-icons must be installed!)
+  (doom-themes-neotree-config)
+  ;; or for treemacs users
+  (setq doom-themes-treemacs-theme "doom-colors") ; use "doom-colors" for less minimal icon theme
+  (doom-themes-treemacs-config)
+  ;; Corrects (and improves) org-mode's native fontification.
+  (doom-themes-org-config))
 
 (use-package ivy
   :diminish
@@ -51,9 +74,51 @@
 	 :map ivy-reverse-i-search-map
 	 ("C-k" . ivy-previous-line)
 	 ("C-d" . ivy-reverse-i-search-kill))
-  :config
+  :init
   (ivy-mode 1))
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map
+	 ("C-r" . 'counsel-minibuffer-history))
+  :config
+  (setq ivy-initial-inputs-alist nil))
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1))
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(doom-themes helpful ivy-rich which-key rainbow-delimiters counsel org ivy doom-modeline catppuccin-theme)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
