9f0393a9366d02fde20e7789f170b5bc36c7463b
Yixu Wang <yxwang@localhost.localdomain>
2024-09-28 15:56:29 +0800

    Move to Org Mode to org config

--- init.el
+++ init.el
@@ -2,30 +2,13 @@
 (when (file-exists-p custom-file)
   (load custom-file))
 
-;; Initialize package sources
-(require 'package)
-
-(setq package-archives '(("melpa" . "https://melpa.org/packages/")
-			 ("org" . "https:/orgmode.org/elpa/")
-			 ("elpa" . "https://elpa.gnu.org/packages/")))
-
-(package-initialize)
-(unless package-archive-contents
-  (package-refresh-contents))
-
-(unless (package-installed-p 'use-package)
-  (package-install 'use-package))
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(setq inhibit-startup-message t)
-
-;; Interface
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (menu-bar-mode -1)
 (tooltip-mode -1)
 (set-fringe-mode 10)
+
+(setq inhibit-startup-message t)
 
 (set-face-attribute 'default nil :font "JetBrainsMonoNL Nerd Font Propo")
 (set-face-attribute 'variable-pitch nil :font "Noto Sans CJK SC")
@@ -44,7 +27,18 @@
 		eshell-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
-(keymap-global-set "<escape>" 'keyboard-escape-quit)
+(require 'package)
+(setq package-archives '(("melpa" . "https://melpa.org/packages/")
+			 ("org" . "https:/orgmode.org/elpa/")
+			 ("elpa" . "https://elpa.gnu.org/packages/")))
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)
 
 (use-package doom-themes
   :ensure t
@@ -64,6 +58,11 @@
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config))
 
+(use-package nerd-icons)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
 (use-package ivy
   :diminish
   :bind (("C-s" . swiper)
@@ -81,30 +80,18 @@
 	 ("C-d" . ivy-reverse-i-search-kill))
   :init
   (ivy-mode 1))
+
 (use-package counsel
   :bind (("M-x" . counsel-M-x)
 	 ("C-x b" . counsel-ibuffer)
 	 ("C-x C-f" . counsel-find-file)
 	 :map minibuffer-local-map
 	 ("C-r" . 'counsel-minibuffer-history))
-  :config
-  (setq ivy-initial-inputs-alist nil))
+  :custom
+  (ivy-initial-inputs-alist nil))
+
 (use-package ivy-rich
   :init (ivy-rich-mode 1))
-
-(use-package nerd-icons)
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1))
-  ;:config
-  ;(set-face-attribute 'mode-line nil
-		      ;:height 120)
-  ;(set-face-attribute 'mode-line-inactive nil
-		      ;:height 120))
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
 
 (use-package which-key
   :init (which-key-mode)
@@ -122,41 +109,13 @@
   ([remap describe-variable] . counsel-describe-variable)
   ([remap describe-key] . helpful-key))
 
-(use-package general
-  :config
-  (general-evil-setup t)
-  (general-create-definer mine/leader-keys
-    :keymaps '(normal insert visual emacs)
-    :prefix "SPC"
-    :global-prefix "C-SPC")
-  (mine/leader-keys
-    "t" '(:ignore t :which-key "toggles")
-    "tt" '(counsel-load-theme :which-key "choose-theme")))
-	       
-(use-package evil
-  :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
-  ;:hook (evil-mode . evil-hook)
-  :config
-  (evil-mode)
-  :custom
-  (evil-respect-visual-line-mode 1)
-  (evil-undo-system 'undo-redo))
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
 (use-package projectile
   :diminish projectile-mode
   :config (projectile-mode)
   :custom ((projectile-completion-system 'ivy))
   :bind-keymap
   ("C-c p" . projectile-command-map))
-  
+
 (use-package magit
   :custom
   (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
@@ -165,12 +124,15 @@
   (org-indent-mode)
   (auto-fill-mode 1)
   (display-line-numbers-mode 0)
-  ;;(visual-line-mode 1)
   (setq evil-auto-intent nil))
+
 (use-package org
   :hook (org-mode . mine/org-mode-setup)
   :custom
-  (org-ellipsis "...")
+  ;; ==============
+  ;; === Agenda ===
+  ;; ==============
+  ;; Basic Setting
   (org-agenda-files
    '("~/Documents/Notes/"))
   (org-agenda-start-with-log-mode t)
@@ -179,24 +141,50 @@
   (org-refile-targets
    '(("archive.org" :maxlevel . 1)))
 
+  ;; Customs
   (org-agenda-custom-commands
    '(("d" "Dashboard" ;;Agenda Dashboard
       ((agenda "" ((org-deadline-warning-days 7)))
        (todo "TODO"
 	     ((org-agenda-overriding-header "All Tasks")))))))
 
-  (org-habit-graph-column 60)
   :config
+  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+
+  ;; =============
+  ;; === Habit ===
+  ;; =============
   (require 'org-habit)
   (add-to-list 'org-modules 'org-habit)
-  (advice-add 'org-refile :after 'org-save-all-org-buffers)
+  (setq org-habit-graph-column 60)
+
+  ;; =============
+  ;; === Tempo ===
+  ;; =============
   (require 'org-tempo)
   (add-to-list 'org-modules 'org-tempo)
-  (add-to-list 'org-structure-template-alist '("el" . "src emacs-lisp"))
+  (add-to-list 'org-structure-template-alist
+		 '("el" . "src emacs-lisp"))
+
+  ;; =============
+  ;; === Babel ===
+  ;; =============
   (org-babel-do-load-languages
    'org-babel-load-languages
    '((emacs-lisp . t)
      (python . t))))
+
+(defun mine/org-babel-tangle-config ()
+  (when (file-equal-p (buffer-file-name)
+		      (expand-file-name "~/.emacs.d/init.org"))
+  (let ((org-confirm-babel-evaluate nil))
+    (org-babel-tangle))))
+
+(add-hook 'org-mode-hook (lambda () (add-hook 'after-save-hook #'mine/org-babel-tangle-config)))
+
+(use-package doom-modeline
+  :ensure t
+  :init (doom-modeline-mode 1))
 
 (defun mine/visual-fill-setup ()
   (setq visual-fill-column-width 80
@@ -215,4 +203,35 @@
   (rime-posframe-properties (list :internal-border-width 10
 				  :font "Noto Sans CJK SC Bold")))
 
+(keymap-global-set "<escape>" 'keyboard-escape-quit)
 
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  :config
+  (evil-mode)
+  :custom
+  (evil-respect-visual-line-mode 1)
+  (evil-undo-system 'undo-redo))
+
+(use-package evil-collection
+  :after evil
+  :config
+  (evil-collection-init))
+
+(use-package general
+  :config
+  (general-evil-setup t)
+  (general-create-definer mine/leader-keys
+    :keymaps '(normal insert visual emacs)
+    :prefix "SPC"
+    :global-prefix "C-SPC")
+
+  (mine/leader-keys
+    "o" '(:ignore o :which-key "Org-mode")
+	"oa" '(org-agenda :which-key "Org-agenda")))
+    "t" '(:ignore t :which-key "toggles")
+    "tt" '(counsel-load-theme :which-key "choose-theme")
