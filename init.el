4f895c3050634525afb9f6aef9f6086f5d978bf0
Yixu Wang <yxwang@localhost.localdomain>
2025-04-17 11:25:22 +0800

    Replace tabspaces with perspective

--- init.el
+++ init.el
@@ -421,41 +421,20 @@ mode to HTML.   Store the result in the clipboard."
 
 (customize-set-variable 'even-window-sizes nil)     ; avoid resizing
 
-(use-package tabspaces
-  :hook (after-init . tabspaces-mode) ;; use this only if you want the minor-mode loaded at startup. 
-  :commands (tabspaces-switch-or-create-workspace
-             tabspaces-open-or-create-project-and-workspace)
+(use-package perspective
+  :bind
+  ("C-x C-b" . persp-list-buffers)
   :custom
-  (tabspaces-use-filtered-buffers-as-default t)
-  (tabspaces-default-tab "Default")
-  (tabspaces-remove-to-default t)
-  (tabspaces-include-buffers '("*scratch*"))
-  (tabspaces-initialize-project-with-todo t)
-  (tabspaces-todo-file-name "project-todo.org")
-  ;; sessions
-  (tabspaces-session t)
-  (tabspaces-session-auto-restore t)
-  (tab-bar-new-tab-choice "*scratch*")
+  (persp-mode-prefix-key (kbd "C-x x"))
+  (persp-state-default-file (expand-file-name "persp-state" user-emacs-directory))
+  :init
+  (persp-mode)
   :config
-  ;; Filter Buffers for Consult-Buffer
-  (with-eval-after-load 'consult
-    ;; hide full buffer list (still available with "b" prefix)
-    (consult-customize consult--source-buffer :hidden t :default nil)
-    ;; set consult-workspace buffer list
-    (defvar consult--source-workspace
-      (list :name     "Workspace Buffers"
-            :narrow   ?w
-            :history  'buffer-name-history
-            :category 'buffer
-            :state    #'consult--buffer-state
-            :default  t
-            :items    (lambda () (consult--buffer-query
-                                :predicate #'tabspaces--local-buffer-p
-                                :sort 'visibility
-                                :as #'buffer-name)))
-
-      "Set workspace buffer list for consult-buffer.")
-    (add-to-list 'consult-buffer-sources 'consult--source-workspace)))
+  (add-hook 'kill-emacs-hook #'persp-state-save)
+  (add-hook 'after-init-hook
+            (lambda () (persp-state-load persp-state-default-file)))
+  (consult-customize consult--source-buffer :hidden t :default nil)
+  (add-to-list 'consult-buffer-sources persp-consult-source))
 
 (use-package rime
   :custom
